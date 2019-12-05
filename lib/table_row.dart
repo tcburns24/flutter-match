f2a670865ee9699bda851420a70d693bc798ac95
Tom Burns <tom@themighty.com>
2019-12-05 11:23:29 -0500

    Adds GameTable class draft 1

--- lib/table_row.dart
+++ lib/table_row.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import './match_card.dart';
 
 class TableRow extends StatelessWidget {
-  TableRow({Key key, this.boardSize}) : super(key: key);
+  TableRow({Key key, this.items}) : super(key: key);
 
-  final int boardSize;
+  final int items;
 
   List<Widget> _generateRow() {
-    return List<Widget>.generate(boardSize, (int index) {
+    return List<Widget>.generate(items, (int index) {
       return MatchCard(text: (index * 10).toString());
     });
   }
@@ -17,6 +17,29 @@ class TableRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: _generateRow(),
+    );
+  }
+}
+
+class GameTable extends StatelessWidget {
+  GameTable({Key key, this.boardSize}) : super(key: key);
+
+  final int boardSize;
+
+  List<Widget> _generateBoard() {
+    return List<Widget>.generate(boardSize, (int index) {
+      return TableRow(items: boardSize);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(padding: EdgeInsets.only(top: 1.0, bottom: 1.0)),
+        TableRow(items: boardSize),
+        Padding(padding: EdgeInsets.only(top: 1.0, bottom: 1.0))
+      ],
     );
   }
 }
